d5e1d7b208c705727f3794955481ade7ebf06c1a
Vishesh <visheshsingh16@gmail.com>
2017-07-31 19:09:08 -0400

    todo-list with delete item event handling

--- src/app/index.js
+++ src/app/index.js
@@ -1,68 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { TodoItem } from './todoItem';
+require('./css/index.css');
 //Create component
 class TodoComponent extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            todos: ['Get up early', 'Mind your work', 'eat', 'sleep', 'repeat'],
-            age: props.age,
             tasks: ['eat', 'sleep', 'conquer', 'repeat']
         }
     }
 
-    //custom functions
-    clicked() {
-        console.log("you clicked me!");
-    }
-
-    makeOlder(){
+    delete(item){
+        var updatedTasks = this.state.tasks.filter((val, index) => {
+            return item !== val;
+        });
         this.setState({
-            age: this.state.age + 3
+            tasks: updatedTasks
         });
     }
 
     render() {
-//Changing the state's age
-        var ager = setTimeout(function() {
-            this.setState({age:30});
-        }.bind(this),3000);
-//Nesting a component
         var tasks = this.state.tasks;
         tasks = tasks.map(function(task, id) {
             return (
-                <TodoItem task={task} key={id} />
+                <TodoItem task={task} key={id} onDelete={this.delete.bind(this)}/>
             );
-        });
+        }.bind(this));
 
         return (
             <div className="container">
-                <div>
-                    <h1>Welcome to ReactJs</h1>
-                    <h3>Here's an example of "props"</h3>
-                    <p><strong>Player name: </strong>{this.props.player.name}</p>
-                    <p><strong>Game: </strong>{this.props.player.game}</p>
-                    <p><strong>Age: </strong>{this.state.age}</p>
-                    <button onClick={this.makeOlder.bind(this)} className="btn btn-success">Make Older!</button>
-                </div>
-                <div id="to-do">
-                    <h3>Here's an example of "state"</h3>
-                    <ul>
-                        <li>This is going to change in 3 secs: {this.state.age}</li>
-                        {this.state.todos.map((todo, id) => <li key={id}>{todo}</li>)}
-                        {/*
-                            <li>{this.state.todos[0]}</li>
-                            <li>{this.state.todos[1]}</li>
-                            <li>{this.state.todos[2]}</li>
-                            <li>{this.state.todos[3]}</li>
-                            <li>{this.state.todos[4]}</li>
-                        */}
-                    </ul>
-                </div>
-                <div>
-                    <h3>Nested component:</h3>
-                    <button onClick={this.clicked} className="btn btn-primary">Click</button>
+                <div id="todo-list">
+                    <p>Todo list for the day:</p>
                     <ul>
                         {tasks}
                     </ul>
@@ -72,13 +41,4 @@ class TodoComponent extends React.Component{
     }//render
 }
 
-const myPlayer = {name: "Messi", game: "Soccer", age: 30};
-
-//propTypes are used for validation and documentation
-TodoComponent.propTypes = {
-    myPlayer: React.PropTypes.object
-}
-
-
-
-ReactDOM.render(<TodoComponent player={myPlayer}/>, document.getElementById('app'));
+ReactDOM.render(<TodoComponent/>, document.getElementById('app'));
--- src/app/todoItem.js
+++ src/app/todoItem.js
@@ -1,12 +1,20 @@
 import React from 'react';
-
+require('./css/todoItem.css');
 //create TodoItem component
 export class TodoItem extends React.Component{
 //custom function
-
+    handleClick(){
+        this.props.onDelete(this.props.task);
+    }
     render(){
         return (
-            <li>{this.props.task} <span className="item-delete">X</span></li>
+            <li>
+                <div className="todo-item">
+                    <span className="item-name"> {this.props.task} </span>
+                    <span onClick={this.handleClick.bind(this)} className="item-remove">X</span>
+                </div>
+            </li>
+
         );
     }
 }
